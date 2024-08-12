4162b63cfb8fdac077f67cee48a6b081b291b032
yaanae <103531280+yaanae@users.noreply.github.com>
2024-08-12 22:04:57 +0200

    Added some options to the module
    Figuring out how submodules (and also dependencies) are
    supposed to be defined was a bit of challenge.
    I realize now though that I have not tested it...
    
    Next stop, actually writing some scripts to make this work at all.

--- module.nix
+++ module.nix
@@ -1,6 +1,12 @@
 {lib, pkgs, config, ...}:
 let 
   cfg = config.services.windows-vsts;
+  
+  defaultWine = pkgs.wine.override {
+    embedInstallers = true; # Mono (and gecko, although we probably don't need that) will be installed automatically
+    wineRelease = "staging"; # Recommended by yabridge
+    wineBuild = "wineWow"; # Both 32-bit and 64-bit wine
+  };
 in
   {
 
@@ -11,6 +17,78 @@ in
       example = true;
       description = "Enable the module";
     };
+
+    # Allow an attribute set for the user defined plugins
+    # The set contains and enabled flag and an installation script string
+    plugins = lib.mkOption {
+      type = lib.types.submodule {
+        options = {
+          enable = lib.types.bool;
+          # Installation script
+          install = lib.types.str;
+          # Extra nix packages to install
+          inputs = lib.types.listOf lib.types.str;
+        };
+      };
+      default = {};
+      example = {
+        "nes-vst" = {
+          enable = true;
+          install = ''
+            unzip ''${inputs.nes-vst}
+            mv "NES VST 1.2.dll" $VST2_DIR
+          '';
+          inputs = [ pkgs.unzip ];
+        };
+      };
+      description = ''
+        A set of user defined plugins to install
+      '';
+    };
+
+    # Allow the user to use custom wine version
+    wine = lib.mkOption {
+      type = lib.types.package;
+      default = defaultWine;
+      example = pkgs.wine.override { embedInstallers = true; };
+      description = ''
+        The wine version to use.
+        "embedInstallers" override in wine is recommended to be set to true
+      '';
+    };
+
+    yabridge = lib.mkOption {
+      type = lib.types.package;
+      default = pkgs.yabridge.override { wine = cfg.wine; };
+      example = pkgs.yabridge.override { wine = cfg.wine; };
+      description = ''
+        The yabridge version to use.
+        "wine" should probably be the same as the "wine" option
+        if you want to use your own yabridge version
+      '';
+    };
+
+    yabridgectl = lib.mkOption {
+      type = lib.types.package;
+      default = pkgs.yabridgectl.override { wine = cfg.wine; };
+      example = pkgs.yabridgectl.override { wine = cfg.wine; };
+      description = ''
+        The yabridgectl version to use.
+        "wine" should probably be the same as the "wine" option
+        if you want to use your own yabridgectl version
+      '';
+    };
+
+    # Allow the user to create a custom wine prefix location
+    prefixPath = lib.mkOption {
+      type = lib.types.path;
+      default = "$HOME/.local/share/nix-vsts";
+      example = "$HOME/.local/share/nix-vsts";
+      description = ''
+        The path to the wine prefix to generate and use
+      '';
+    };
+
   };
 
   config = lib.mkIf cfg.enable {
