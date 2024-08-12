f9b2c08aa529667b439396966a46ee6ffc20b79a
yaanae <103531280+yaanae@users.noreply.github.com>
2024-08-12 13:59:15 +0200

    Trying to install all the VSTs into a wine prefix.

---
+++ flake.nix
@@ -0,0 +1,54 @@
+{
+  description = "Automatically wrap your Windows VSTs with yabridge";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nes-vst = {
+      url = "https://www.mattmontag.com/nesvst/NES-VST-1.2.zip";
+      flake = false;
+    };
+    poise = {
+      url = "https://osc.sfo2.digitaloceanspaces.com/Setup_Poise_64bit_1-1-55-6_Windows_Full.exe";
+      flake = false;
+    };
+  };
+
+  outputs = { self, nixpkgs, ... }@inputs:
+  let 
+    pkgs = nixpkgs.legacyPackages.x86_64-linux;
+
+    wine = pkgs.wine;
+    winetricks = pkgs.winetricks;
+    yabridge = pkgs.yabridge;
+    yabridgectl = pkgs.yabridgectl;
+    poise = inputs.poise;
+    nes-vst = inputs.nes-vst;
+
+    setup-vsts-script = pkgs.writeShellScriptBin "setup-windows-vsts" ''
+        # This script is run the first time the package is installed
+        # It should install the VSTs
+
+        # Setting up the wine prefix
+        export WINEPREFIX="$HOME/.wine-nix/setup-windows-vsts"
+        mkdir -p "$WINEPREFIX"
+
+        # Prepare file structure
+        export VST_PATH="$WINEPREFIX/drive_c/Program Files/Steinberg/VstPlugins"
+        mkdir -p "$VST_PATH"
+        
+        cp "${nes-vst}/NES VST 1.2.dll" "$VST_PATH"
+        wine ${poise}/Setup_Poise_64bit_1-1-55-6_Windows_Full.exe 
+
+        # Let yabridge know where the VSTs are
+        yabridgectl add "$VST_PATH"
+      
+    '';
+    setup-vsts = pkgs.symlinkJoin {
+      name = "setup-windows-vsts";
+      paths = [ yabridge yabridgectl setup-vsts-script winetricks ];
+    };
+
+  in {
+    packages.x86_64-linux.setup-vsts = setup-vsts;
+  };
+}
